80f613167431621e5627231fc789104d08e726c9
WastPow <48478380+WastPow@users.noreply.github.com>
2021-07-06 11:13:34 +0200

    Add files via upload

---
+++ keyworder.py
@@ -0,0 +1,44 @@
+def keyworder(text):
+    tags = {
+        "#kost": [
+            "kost",
+            "mat",
+            "matvana",
+            "läsk",
+            "socker",
+            "kalorier",
+            "saft",
+            "onyttig",
+            "fett",
+            "kolhydrater",
+            "cola",
+            "godis"
+        ]
+    }
+
+    article = text.split(". ")
+    appropriate_tags = []
+    flag = 0
+
+    for tag in tags:
+        for keyword in tags[tag]:
+            if keyword in text:
+                appropriate_tags.append(tag)
+                flag = 1
+                for sentence in article:
+                    if keyword in sentence:
+                        print(sentence)
+
+    # checking condition for string found or not
+    if flag == 1:
+        print("Appropriate tags: {}".format(str(appropriate_tags)))
+    else:
+        print("Didn't find any appropriate tags to assign")
+
+
+art = "".lower()
+
+keyworder(art)
+
+
+
